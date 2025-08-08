75332d3d4c7e5f772b3ca88f65db83d4254cbc14
Asura641 <youremail@example.com>
2025-08-08 19:13:42 +0530

    Auto update 2025-08-08 19:13:41

--- auto_push.py
+++ auto_push.py
@@ -38,6 +38,8 @@ def list_repo_files():
         all_files = untracked_files.splitlines()
         untracked_list = []
         repo_path_len = len(REPO_PATH.replace('/', '\\')) + 1 # +1 for the trailing slash
+        print(f"All files from PowerShell (all_files): {all_files}")
+        print(f"Tracked files from Git (tracked_list): {tracked_list}")
         for f_abs in all_files:
             f_rel = f_abs[repo_path_len:]
             # Exclude .git folder and files already in the java subdirectory
