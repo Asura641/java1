1bfc88c0db09995233bd5b77e2b5fdc61db1abd4
Asura641 <youremail@example.com>
2025-08-08 15:39:11 +0530

    Auto update 2025-08-08 15:39:11

--- BuitInPackages.java
+++ BuitInPackages.java
@@ -1,13 +1,14 @@
 import java.util.Scanner ;
 
 class BuitInPackages{
-public static void main(String [] args){
+public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
 	
 	System.out.print("Enter Yor name : ");
         String Name = sc.nextLine();
 	
-	System.out.println("Hello ,"+name+"!");
+	System.out.println("Hello," + Name + "!");
+	sc.close();
 }
 
 
