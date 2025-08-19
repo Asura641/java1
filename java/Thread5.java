7690d973588fbd04ca3f7d74af99db162cd803c0
Asura641 <youremail@example.com>
2025-08-19 16:03:28 +0530

    Auto update 2025-08-19 13:32:05

---
+++ java/Thread5.java
@@ -0,0 +1,11 @@
+class Thread5 implements Thread{
+	public void run(){
+	System.out.println("Thread running");
+}
+public static void main(String [] args ){
+	Thread5 th =new Thread5 ();
+	th.start();
+}
+}
+
+
