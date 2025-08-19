d24162fe9298df86467ed62e5724d36c2c47d60d
Asura641 <youremail@example.com>
2025-08-19 15:57:48 +0530

    Auto update 2025-08-19 13:32:05

---
+++ java/Thread4.java
@@ -0,0 +1,11 @@
+class Thread4 extends Thread{
+	void run(){
+	System.out.println("Thread running");
+public static void main(String [] args ){
+	Thread4 th =new Thread4 ()
+	th.start();
+}
+}
+
+
+}
